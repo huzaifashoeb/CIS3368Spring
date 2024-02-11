2874be0ee847602ed86f94d71ec4ea3e6dbd1f0f
huzaifashoeb <143118571+huzaifashoeb@users.noreply.github.com>
2024-02-10 22:19:01 -0600

    Update HW1.py
    Menu working
    Order working
    Information working
    receipt working

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -2,6 +2,7 @@
 #Following is the Python Code for Homework 1
 
 import mysql.connector
+from mysql.connector import Error
 
 # Connect to the MySQL database
 db_connection = mysql.connector.connect(
@@ -11,44 +12,85 @@ db_connection = mysql.connector.connect(
     database="cis3368springdb"
 )
 
-# Function to display all the drinks in database
+#Function for Displaying the menu of drinks and the information
 def display_drinks(cursor):
     cursor.execute("SELECT id, Drink, Price FROM Drinks")
     drinks = cursor.fetchall()
-    
+
     for drink in drinks:
         print(f"{drink[0]} - {drink[1]}: ${drink[2]}")
 
-# Function to get drink information
 def get_drink_info(cursor, drink_id):
     cursor.execute("SELECT Description, Color FROM Drinks WHERE id = %s", (drink_id,))
     drink_info = cursor.fetchone()
-    
+
     if drink_info:
         print(f"Description: {drink_info[0]}")
         print(f"Color: {drink_info[1]}")
     else:
-        print("Sorry, Drink not found in menu")
+        print("Sorry, Drink not found in the menu")   #If a number is entered which is not found in menu
 
-# Function for placing the order
 def place_order(cursor):
     order = []
-    
+    drink_info_requested = False  # This Boolean is to check if the user requested information on a drink
+
     while True:
-        display_drinks(cursor)
-        answer = input("Welcome! Are you ready to place an order? (yes/no): ").upper()
-        
-        if answer == 'NO':
+        if not drink_info_requested:
+            display_drinks(cursor)
+
+        answer = input("Welcome! Would you like information about a drink from our menu or to place an order? (i = Description / o = Order / c = Checkout): ").upper()
+#Using the .upper() function to make sure user input is not case sensitive
+        if answer == 'C':
             break
-        elif answer == 'YES':
+        elif answer == 'I':
+            drink_id = int(input("Certainly! Please enter the drink number you would like the description of: "))
+            get_drink_info(cursor, drink_id)
+            drink_info_requested = True   #This Boolean helps to hide the menu and not repeat it after each step of order
+        elif answer == 'O':
             drink_id = int(input("Please enter the drink number you would like to order from the menu: "))
             get_drink_info(cursor, drink_id)
-            
+
             quantity = int(input("Please enter how many servings you would like: "))
             order.append((drink_id, quantity))
-            
-            add_another = input("Would you like to add another drink to your order? (yes/no): ").upper()
-            if add_another != 'YES':
-                break
+
+# Ask user if they want to add anyhting else to the order
+            add_another = input("Would you like to add another drink to your order? (y = yes / n = no): ").upper()
+            if add_another != 'Y':order
+            drink_info_requested = False  # Display the menu again if the order is completed 
 
     return order
+
+def print_receipt(order, cursor):
+    total = 0
+
+# Reciept for the user
+    print("\nReceipt:")
+    for items in order:
+        drink_id, quantity = items
+        cursor.execute("SELECT Drink, Price FROM Drinks WHERE id = %s", (drink_id,))
+        drink_info = cursor.fetchone()
+
+        if drink_info:
+            drinkname, price = drink_info
+            total += price * quantity
+            print(f"{quantity} x {drinkname}: ${price * quantity}")
+
+    print(f"\nTotal: ${total:.2f}")
+
+try:
+    cursor = db_connection.cursor()
+
+    while True:
+        order_or_info = input("Welcome! Would you like information about a drink from our menu or to place an order? (i = Description / o = Order / c = Checkout): ").upper()
+
+        if order_or_info == 'C':
+            break
+        elif order_or_info == 'I':
+            drink_id = int(input("Please enter the drink number you would like information about: "))
+            get_drink_info(cursor, drink_id)
+        elif order_or_info == 'O':
+            order = place_order(cursor)
+            print_receipt(order, cursor)
+
+except mysql.connector.Error as e:
+    print(f"Error: {e}")
