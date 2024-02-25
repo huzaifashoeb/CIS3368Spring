8e4950b41a77636c6147f2d2af2897c33ba2f27a
huzaifashoeb <143118571+huzaifashoeb@users.noreply.github.com>
2024-02-24 22:54:42 -0600

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -14,3 +14,29 @@ db_config = {
     'database': 'cis3368springdb'
 
 }
+
+# Function to execute SQL read query
+def sql_read_query(query, params=()):
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute(query, params)
+    result = cursor.fetchall()
+    conn.close()
+    return result
+
+# Function to execute SQL write query
+def sql_write_query(query, params=()):
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor()
+    cursor.execute(query, params)
+    conn.commit()
+    conn.close()
+
+# API Endpoint to get all tires from Inventory
+@app.route('/api/Inventory', methods=['GET'])
+def get_Inventory():
+    query = 'SELECT * FROM Inventory'
+    tires = sql_read_query(query)
+
+    return jsonify({'Inventory': tires})
+
